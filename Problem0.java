4c81d1fe769eb41fd84d88d1d7747318bd75aa1b
khangv9342 <kmvo100@student.hbuhsd.edu>
2024-09-10 10:16:55 -0700

    gyat

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,12 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    double dubVar = 3;
+    int singleVar = 4.00;
+
+    System.out.print(dubVar);
+    System.out.print(singleVar);
+
+    
   }
 }
